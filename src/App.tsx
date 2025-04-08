3f241aaeb085c603ca22eae7900dc8f04bd6d016
Bench-amblee <benjamin.t.chamblee@gmail.com>
2025-04-07 22:48:08 -0500

    removed footer

--- src/App.tsx
+++ src/App.tsx
@@ -118,15 +118,6 @@ function App() {
           />
         </div>
       </div>
-
-      {/* Footer */}
-      <footer className="bg-gray-50 border-t border-gray-200 mt-20">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <div className="text-center text-gray-600">
-            <p>© 2024 AlignRFP. All rights reserved.</p>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 }
